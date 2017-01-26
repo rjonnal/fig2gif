9ea6e4df321a5eb8c6194d24dbda9d2caa70866f
rjonnal <rjonnal@gmail.com>
2017-01-26 14:40:11 -0800

    added __del__ function for automatic cleanup of working directory

--- __init__.py
+++ __init__.py
@@ -17,16 +17,30 @@ class GIF:
         while os.path.exists(self.wdir):
             self.wdir = '%s_%d.tmp'%(self.gif_filename,random.randint(0,2**32))
 
-        logging.info('Creating temporary directory %s.'%self.wdir)
+        self.logger.info('Creating temporary directory %s.'%self.wdir)
         os.mkdir(self.wdir)
             
         self.index = 0
         self.dpi = dpi
         
+    def __del__(self):
+        if os.path.exists(self.wdir):
+            try:
+                self.logger.info('abnormal exit, deleting %s.'%self.wdir)
+                png_list = glob.glob(os.path.join(self.wdir,'*.png'))
+                for png_file in png_list:
+                    os.remove(png_file)
+
+                # clean up working directory
+                os.rmdir(self.wdir)
+                
+            except Exception as e:
+                print e
+                
     def add(self,fig):
         # save the given figure to the working directory
         outfn = os.path.join(self.wdir,'frame_%020d.png'%self.index)
-        logging.info('Saving figure to file %s.'%outfn)
+        self.logger.info('Saving figure to file %s.'%outfn)
         fig.savefig(outfn,dpi=self.dpi)
         self.index = self.index + 1
 
@@ -36,11 +50,11 @@ class GIF:
         delay = 1.0/float(self.fps)*100.0
 
         # run ImageMagick convert function to make the GIF
-        logging.info('Running ImageMagick convert to create gif in %s.'%self.gif_filename)
+        self.logger.info('Running ImageMagick convert to create gif in %s.'%self.gif_filename)
         command = ['convert','-delay','%0.1f'%delay,'-loop','%d'%self.loop,'%s'%(os.path.join(self.wdir,'frame*.png')),'%s'%self.gif_filename]
         call(command)
 
-        logging.info('Cleaning up.')
+        self.logger.info('Cleaning up.')
         # clean up temporary png files
         png_list = glob.glob(os.path.join(self.wdir,'*.png'))
         for png_file in png_list:
